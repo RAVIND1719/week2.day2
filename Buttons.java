01740f30074b79e2312ab5391c13a1710738ab01
RAVIND1719 <99536592+RAVIND1719@users.noreply.github.com>
2022-05-03 18:49:59 +0530

    Create Buttons.java

---
+++ Buttons.java
@@ -0,0 +1,22 @@
+package assignments_w2_day2;
+
+
+import org.openqa.selenium.By;
+	import org.openqa.selenium.chrome.ChromeDriver;
+
+	import io.github.bonigarcia.wdm.WebDriverManager;
+
+	public class Buttons {
+		public static void main(String[] args) {
+			WebDriverManager.chromedriver().setup();
+		    ChromeDriver driver = new ChromeDriver();
+		    driver.get("http://leafground.com/pages/Button.html");
+            System.out.println(driver.findElement(By.id("position")).getLocation());
+		    System.out.println( driver.findElement( By.id("color")).getCssValue("background-color"));
+		    System.out.println(driver.findElement(By.id("size")).getSize());
+		    driver.findElement(By.xpath("//button[@id='home']")).click();
+		}
+
+	}
+
+
